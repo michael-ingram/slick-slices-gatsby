45f6f0545d3afafd5e680053c47da73c57194345
Michael Ingram <mingram@michaels-air.lan>
2022-02-21 13:10:39 -0700

    first commit

---
+++ gatsby/src/pages/pizzas.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../components/Nav';
+import Layout from '../components/Layout';
+
+export default function PizzasPage() {
+  return (
+    <>
+      <p>Hey I'm the Pizzas Page</p>
+    </>
+  );
+}
