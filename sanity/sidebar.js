bbc693205577fc5e2b90205dfce30bf376d7d3a6
Michael Ingram <mingram@michaels-air.lan>
2022-04-21 09:50:01 -0600

    ready for deploy

---
+++ sanity/sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import S from '@sanity/desk-tool/structure-builder';
+
+export default function Sidebar() {
+  return S.list()
+    .title(`Slick's Slices`)
+    .items([S.listItem().title('Home Page')
+        .icon(() => <strong>H</strong>)
+        .child(S.editor().schemaType('storeSettings').documentId('downtown')),
+      ...S.documentTypeListItems().filter((item) => item.getId() !== 'storeSettings'),
+    ]);
+}
